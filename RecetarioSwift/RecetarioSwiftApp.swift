f7911d3de9ae7f8db0066539d1f956ca1177c3a0
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-01-30 17:31:11 -0600

    Creamos projecto base

---
+++ RecetarioSwift/RecetarioSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecetarioSwiftApp.swift
+//  RecetarioSwift
+//
+//  Created by Rodolfo Castillo on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RecetarioSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
